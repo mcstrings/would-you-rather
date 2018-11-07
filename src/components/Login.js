e1fc22770bdaf31a4cf7c12c6722365c6b28a406
mcstrings <garyarthuryoung@gmail.com>
2018-11-06 19:39:55 -0500

    fix: Sort users alphabetically

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,6 @@ import { setAuthedUser } from '../actions/authedUser'
 import { getArrayFromObj } from '../utils'
 
 class Login extends Component {
-
     handleSelectUserChange = (e) => {
         const { dispatch } = this.props
         dispatch(setAuthedUser(e.target.value))
@@ -20,13 +19,24 @@ class Login extends Component {
                     value={authedUserID}
                     onChange={this.handleSelectUserChange}
                 >
+                    {/* Sort users alphabetically */}
                     <option value="">Select a user login</option>
                     {users &&
-                        getArrayFromObj(users).map((user) => (
-                            <option key={user.id} value={user.id}>
-                                {user.name}
-                            </option>
-                        ))}
+                        getArrayFromObj(users)
+                            .sort((a, b) => {
+                                const nameA = a.name.toUpperCase()
+                                const nameB = b.name.toUpperCase()
+                                return nameA < nameB
+                                    ? -1
+                                    : nameA > nameB
+                                        ? 1
+                                        : 0
+                            })
+                            .map((user) => (
+                                <option key={user.id} value={user.id}>
+                                    {user.name}
+                                </option>
+                            ))}
                 </select>
             </Fragment>
         )

1b58e4eaba9d12c79cbedc6db91930b9aca672f6
mcstrings <garyarthuryoung@gmail.com>
2018-11-18 21:37:49 -0500

    feat: Create the 404 Error component

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { Jumbotron } from 'react-bootstrap'
 
 export default class PageNotFound extends Component {
-  render() {
-    return (
-      <h1>
-          404 Page not found
-      </h1>
-    )
-  }
+    render() {
+        return (
+            <Jumbotron className="m-3">
+                <span className="text-danger">Error</span>{' '}
+                <span className="display-1 text-danger">404</span>
+                <hr />
+                <p className="display-4">
+                    You have to be logged in to view this page.
+                </p>
+            </Jumbotron>
+        )
+    }
 }

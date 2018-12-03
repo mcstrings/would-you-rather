be32255ba9fd578d3359a7705bb6f13564c4e9c4
mcstrings <garyarthuryoung@gmail.com>
2018-12-03 12:09:50 -0500

    Updates

---
+++ precache-manifest.bce8b0052d8d601875aea287e09c388a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f522f220959f593645c",
+    "url": "/would-you-rather/static/js/runtime~main.5f522f22.js"
+  },
+  {
+    "revision": "50702b77a29e040f5a44",
+    "url": "/would-you-rather/static/js/main.50702b77.chunk.js"
+  },
+  {
+    "revision": "148ef2687e952a3b67ff",
+    "url": "/would-you-rather/static/js/1.148ef268.chunk.js"
+  },
+  {
+    "revision": "50702b77a29e040f5a44",
+    "url": "/would-you-rather/static/css/main.8716f4a6.chunk.css"
+  },
+  {
+    "revision": "148ef2687e952a3b67ff",
+    "url": "/would-you-rather/static/css/1.2ba0183c.chunk.css"
+  },
+  {
+    "revision": "c354b6da8ecf1b6f5427c0b94c3230e0",
+    "url": "/would-you-rather/index.html"
+  }
+];
